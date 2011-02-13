da69a53c16536d067798abdb8e2160e0a7b3ed96
Nick Sieger <nick@nicksieger.com>
2011-02-14 00:59:11 +0100

    Add a fresh, empty Rails 3 application with no ORM

---
+++ README.md
@@ -0,0 +1,7 @@
+=== Rails 3 + Spring Petclinic
+
+This project showcases how you might use Rails 3 along with
+Spring+Hibernate and existing Java sources from the [Spring
+Petclinic][pc] application.
+
+[pc]: http://static.springsource.org/docs/petclinic.html
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpringPetclinic::Application.config.secret_token = '2e03aaffb5304037dd1e15a41fa0d986274354d79533a521e757c2e9f0174a15bbda48bf956f0d47558a117a66117dc6977eb263ce0221c0b88c001e29b4c4f0'
