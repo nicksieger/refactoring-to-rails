8284231286abdc9b620c00b71ba3460c513f6f72
Nick Sieger <nick@nicksieger.com>
2011-02-14 23:19:14 +0100

    Tweak validation message, and eliminate extraneous debug statement

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -7,9 +7,7 @@ class Owner
   include Spring
 
   validates_presence_of :first_name, :last_name, :address, :city, :telephone
-  validates_format_of :telephone, :with => /[-0-9. ]+/, :message => "Only numbers, dash, dot (.), or spaces allowed"
-
-  p _validators
+  validates_format_of :telephone, :with => /[-0-9.+ ]+/, :message => "can only have numbers, punctuation, or spaces"
 
   def self.find_by_name(name)
     clinic.findOwners(name)
