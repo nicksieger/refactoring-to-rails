4a2d3fc81acc285be90284831d2bc74efb7177bd
Nick Sieger <nick@nicksieger.com>
2011-04-04 12:32:48 -0500

    Implement /rack/vets.json in the Sinatra app
    We're using the `json` gem that we added to our `Gemfile` earlier.
    Generating JSON is straightforward -- we obtain a list of vets from
    the clinic object and map it to an array of Ruby hashes.
    
    `rake cucumber:extended` should now pass.
    
        14 scenarios (14 passed)
        79 steps (79 passed)

--- src/main/webapp/WEB-INF/lib/app.rb
+++ src/main/webapp/WEB-INF/lib/app.rb
@@ -1,5 +1,6 @@
 require 'builder'
 require 'erb'
+require 'json'
 require 'spring_helpers'
 require 'java_ext'
 
@@ -31,6 +32,20 @@ get '/rack/vets.xml' do
       end
     end
   end
+end
+
+get '/rack/vets.json' do
+  content_type 'application/json'
+  JSON.pretty_generate(clinic.vets.map do |vet|
+    { "id" => vet.id,
+      "firstName" => vet.firstName,
+      "lastName" => vet.lastName
+    }.tap do |h|
+      unless vet.specialties.empty?
+        h["specialties"] = vet.specialties.map {|s| { "id" => s.id, "name" => s.name } }
+      end
+    end
+  end)
 end
 
 get '/rack/owners/:owner/pets/:pet/visits.atom' do |owner_id, pet_id|
