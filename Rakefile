cce31e25fcce1cb06a67c1483c2f5c8e05d4ae3c
Nick Sieger <nick@nicksieger.com>
2011-04-08 18:15:32 +0100

    Add empty Rails application at the root of the project
    We're now going to embark on the "large" refactoring. What we consider
    makes it "large" is that it's potentially more disruptive to your
    existing Java development workflow. We'll aim to run the entire
    application inside a `rails server` process. Changing the project
    structure to mirror a regular Rails application allows us to use
    generators and other Rails tools.
    
    This commit is also a bit bigger because we'll need a couple more
    things in place before we can resume running the Cucumber features,
    Then, once we do, we'll immediately be staring at a bunch of failures
    because we haven't implemented the rest of the application yet.

--- Rakefile
+++ Rakefile
@@ -1,8 +1,7 @@
-begin
-  require 'rubygems'
-  require 'bundler/setup'
-rescue LoadError
-  fail "Please 'gem install bundler' and 'bundle install' first."
-end
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
-FileList['lib/tasks/*.rake'].each {|f| import f }
+require File.expand_path('../config/application', __FILE__)
+require 'rake'
+
+SpringPetclinic::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpringPetclinic::Application.config.secret_token = '67e6b3dd0d831badf25a4a24ef1911f37cd474f2d172960daa630f2725a5ed1b89f4b0352d5d0a4e10e59599a3d8dce63f65e78c90672fbd9353a7eace783d56'
