b036a4ad9901d75f9aecda4eab170171b09802d6
Nick Sieger <nick@nicksieger.com>
2011-04-08 18:47:03 +0100

    Move Rails code, assets and config into place
    We're reclaiming the Rails code we already wrote and moving it to the
    proper place in `app/` and `lib/`. We also moved images and
    stylesheets, as well as helper code.
    
    The application still doesn't really run, because there's no Spring
    context -- we need to boot it ourselves, since there's no container to
    do it for us. That Spring configuration will come in the next commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 SpringPetclinic::Application.routes.draw do
+  resources :vets
+
+  get 'owners/:owner_id/pets/:id/visits.:format' => 'visits#index'
+
+  get 'owners/search' => 'owners#search'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ SpringPetclinic::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
--- src/main/webapp/WEB-INF/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SpringPetclinic::Application.config.secret_token = 'f783c73977dc31b9df955257abe65fae9df39bc5081de9e474dac6a717320421564401f7cd8eb876d577955f819a4750bea08e53c91f18f7c1715568a4ace001'
